8b2b1357a172d5f16d94f2596f8603a72feb601c
Bidisha Das <officialbidisha1@gmail.com>
2024-12-05 15:43:29 +0530

    Update progressMem.js

--- progressMem.js
+++ progressMem.js
@@ -1,4 +1,4 @@
-/**
+/*
 
 
 memoize is a function that takes one argument, asynchronousFunction:
@@ -24,38 +24,33 @@ Implement memoize.
 User Code
 function slowFunction(param1, param2, callback) {
   setTimeout(function respond() {
-    callback(null /* no error */, {
-      firstParameter: param1,
-      secondParameter: param2 // This can be anything, as long as it depends on the passed parameters
-    });
-  }, 1000 /* make it slow */);
-}
+// callback(null /* no error */, {
+//       firstParameter: param1,
+//       secondParameter: param2 // This can be anything, as long as it depends on the passed parameters
+//     });
+//   }, 1000 /* make it slow */);
+// }
 
-slowFunction('foo', 'bar', function onFirstInvocation(error, data) {
-  // Should take 1 second
-  console.log(data);
-  slowFunction('foo', 'bar', function onSecondInvocation(error, data) {
-    // Should take 1 second
-    console.log(data);
-  });
-});
+// slowFunction('foo', 'bar', function onFirstInvocation(error, data) {
+//   // Should take 1 second
+//   console.log(data);
+//   slowFunction('foo', 'bar', function onSecondInvocation(error, data) {
+//     // Should take 1 second
+//     console.log(data);
+//   });
+// });
 
-var fastFunction = memoize(slowFunction);
+// var fastFunction = memoize(slowFunction);
 
-fastFunction('foo', 'bar', function onFirstInvocation(error, data) {
-  // Should take 1 second
-  console.log(data);
-  fastFunction('foo', 'bar', function onSecondInvocation(error, data) {
-    // Should be immediate / very fast!
-    console.log(data);
-  });
-});
-
-
-
-
-
-*/
+// fastFunction('foo', 'bar', function onFirstInvocation(error, data) {
+//   // Should take 1 second
+//   console.log(data);
+//   fastFunction('foo', 'bar', function onSecondInvocation(error, data) {
+//     // Should be immediate / very fast!
+//     console.log(data);
+//   });
+// });
+// 
 
 
 
