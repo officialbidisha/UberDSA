f96e3db09733732e1955dbac5f519418fac55a91
Bidisha Das <officialbidisha1@gmail.com>
2024-12-05 23:09:37 +0530

    Update progressMem.js

--- progressMem.js
+++ progressMem.js
@@ -58,99 +58,117 @@ function slowFunction(param1, param2, callback) {
 
 
 function memoize(fn) {
-  let cache = {};  // Cache to store results
-
+  let cache = {};
   return function memoized(...args) {
-    let cb = args[args.length - 1];  // The callback is always the last argument
-    let params = args.slice(0, -1);  // The function arguments without the callback
-    let key = generateKey(params);  // Generate a cache key from the parameters
-
-    // If the result is already in the cache, return it immediately
+    let params = args.slice(0, args.length - 1);
+    let cb = args[args.length - 1];
+    let key = generateKey(params);
     if (cache.hasOwnProperty(key)) {
+      console.log("---RETURNED FROM CACHE---");
       return cb(null, cache[key]);
     }
-
-    // If not cached, invoke the original function
     fn(...params, (err, data) => {
       if (err) {
         return cb(err, null);
+      } else {
+        cache[key] = data;
+        cb(null, data);
       }
-
-      // Cache the result
-      cache[key] = data;
-      cb(null, data);
     });
   };
-}
 
-// Function to generate a unique key for the cache based on arguments
-function generateKey(args) {
-  return args.map(arg => {
-    if (arg === null) return 'null';
-    if (typeof arg === 'undefined') return 'undefined';
-    if (typeof arg === 'function') return arg.toString(); // Serialize function
-    if (typeof arg === 'object') return stableStringify(arg); // Use stable stringify for objects
-    return String(arg); // Handle primitives
-  }).join('|'); // Join with a separator
-}
-
-// Stable stringify function to ensure consistent key generation
-function stableStringify(obj) {
-  if (obj === null) return 'null';
-  if (typeof obj !== 'object') return String(obj);
-  
-  const keys = Object.keys(obj).sort(); // Sort keys to maintain order
-  const sortedObj = {};
-  for (const key of keys) {
-    sortedObj[key] = stableStringify(obj[key]); // Recursively stringify values
+  function generateKey(args) {
+    return args
+      .map((arg) => {
+        if (typeof arg === "object") {
+          return handleObject(arg);
+        }
+        if (typeof arg === "function") {
+          return arg.toString();
+        }
+        if (typeof arg === "undefined") return "undefined";
+        if (arg === null) return "null";
+        return String(arg);
+      })
+      .sort()
+      .join("|");
   }
-  return JSON.stringify(sortedObj); // Convert to string
+
+  function handleObject(obj) {
+    if (obj === null) return "null";
+    if (typeof obj !== "object") return String(obj);
+    const keys = Object.keys(obj).sort();
+    let sortedObj = {};
+    for (const key of keys) {
+      sortedObj[key] = stableStringify(obj[key]); // Recursively process values
+    }
+    return JSON.stringify(sortedObj); // Convert sorted object to string
+  }
 }
 
-// Example of a slow function
-function slowFunction(param1, param2, callback) {
-  setTimeout(function respond() {
-    callback(null, {
-      firstParameter: param1,
-      secondParameter: param2 // This can be anything, as long as it depends on the passed parameters
-    });
-  }, 1000); // Simulate a delay
-}
+// function slowFunction(param1, param2, callback) {
+//   setTimeout(() => {
+//     callback(null, {
+//       firstParameter: param1,
+//       secondParameter: param2, // Result depends on inputs
+//     });
+//   }, 4000); // Simulate a 4-second delay
+// }
 
-// Testing the memoize function
-let fastFunction = memoize(slowFunction);
+// let fastFunc = memoize(slowFunction);
+// fastFunc("foo", "bar", function onFirstInvocation(error, data) {
+//   // Should take 1 second
+//   console.log(data);
+//   fastFunc("bar", "foo", function onSecondInvocation(error, data) {
+//     // Should be immediate / very fast!
+//     console.log(data);
+//   });
+// });
 
-fastFunction('foo', 'bar', function onFirstInvocation(error, data) {
-  // Should take 1 second
-  console.log('First call:', data);
-  fastFunction('foo', 'bar', function onSecondInvocation(error, data) {
-    // Should be immediate / very fast!
-    console.log('Second call:', data);
-  });
-});
+// // Example of a slow function
+// function slowFunction(param1, param2, callback) {
+//   setTimeout(function respond() {
+//     callback(null, {
+//       firstParameter: param1,
+//       secondParameter: param2 // This can be anything, as long as it depends on the passed parameters
+//     });
+//   }, 1000); // Simulate a delay
+// }
 
-// Testing another scenario
-function getSquare(x, cb) {
-  console.log("function executed", x);
-  setTimeout(() => {
-    cb(null, x * x);
-  }, 200);
-}
+// // Testing the memoize function
+// let fastFunction = memoize(slowFunction);
 
-let memoizedSquare = memoize(getSquare);
+// fastFunction('foo', 'bar', function onFirstInvocation(error, data) {
+//   // Should take 1 second
+//   console.log('First call:', data);
+//   fastFunction('foo', 'bar', function onSecondInvocation(error, data) {
+//     // Should be immediate / very fast!
+//     console.log('Second call:', data);
+//   });
+// });
 
-memoizedSquare(4, (err, res) => {
-  if (!err) console.log('Square of 4:', res); // Should print 16
-});
-memoizedSquare(4, (err, res) => {
-  if (!err) console.log('Square of 4 again:', res); // Should print 16 (cached)
-});
-memoizedSquare(5, (err, res) => {
-  if (!err) console.log('Square of 5:', res); // Should print 25
-});
-memoizedSquare(6, (err, res) => {
-  if (!err) console.log('Square of 6:', res); // Should print 36
-});
-memoizedSquare(4, (err, res) => {
-  if (!err) console.log('Square of 4 again:', res); // Should print 16 (cached)
-});
+// // Testing another scenario
+// function getSquare(x, cb) {
+//   console.log("function executed", x);
+//   setTimeout(() => {
+//     cb(null, x * x);
+//   }, 200);
+// }
+
+// let memoizedSquare = memoize(getSquare);
+
+// memoizedSquare(4, (err, res) => {
+//   if (!err) console.log('Square of 4:', res); // Should print 16
+// });
+// memoizedSquare(4, (err, res) => {
+//   if (!err) console.log('Square of 4 again:', res); // Should print 16 (cached)
+// });
+// memoizedSquare(5, (err, res) => {
+//   if (!err) console.log('Square of 5:', res); // Should print 25
+// });
+// memoizedSquare(6, (err, res) => {
+//   if (!err) console.log('Square of 6:', res); // Should print 36
+// });
+// memoizedSquare(4, (err, res) => {
+//   if (!err) console.log('Square of 4 again:', res); // Should print 16 (cached)
+// });
