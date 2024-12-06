ddb67208b6bd1f218eb5fef2fcffae2766345362
Bidisha Das <officialbidisha1@gmail.com>
2024-12-06 12:28:08 +0530

    Update progressMem.js

--- progressMem.js
+++ progressMem.js
@@ -55,6 +55,76 @@ function slowFunction(param1, param2, callback) {
 // });
 // 
 
+function memoize(fn) {
+  let cache = {}; // Cache to store results
+  let progressQueues = {}; // Store pending callbacks for each unique key
+
+  return function memoized(...args) {
+    let cb = args[args.length - 1]; // The last argument is the callback
+    let params = args.slice(0, -1); // All arguments except the callback
+    let key = generateKey(params); // Generate a unique key based on arguments
+
+    // Check if result is already in cache
+    if (cache.hasOwnProperty(key)) {
+      
+       cb(null, cache[key]); // Return the cached result
+       return;
+    }
+
+    // If no result in cache, check if the function is already being processed
+    if (!progressQueues.hasOwnProperty(key)) {
+      progressQueues[key] = [cb]; // Initialize the queue with the current callback
+    } else {
+      progressQueues[key].push(cb); // Add the callback to the queue if already in progress
+      return; // Exit early since the function is already processing for these args
+    }
+
+    // Execute the function to compute the result
+    fn(...params, (err, data) => {
+    console.log('Executed');
+      if (err) {
+        // If there's an error, call all queued callbacks with the error
+        progressQueues[key].forEach((queuedCb) => queuedCb(err, null));
+      } else {
+        // Cache the result
+        cache[key] = data;
+        // Call all queued callbacks with the result
+        progressQueues[key].forEach((queuedCb) => queuedCb(null, data));
+      }
+      // Clean up the queue after the result is processed
+      delete progressQueues[key];
+    });
+  };
+}
+
+// Example async function to be memoized
+function getSquare(x, cb) {
+  console.log('function executed', x);
+  setTimeout(() => {
+    cb(null, x * x); // Return square of x after 200ms
+  }, 200);
+}
+
+// Testing the memoized function with callbacks
+let memoizedSquare = memoize(getSquare);
+
+memoizedSquare(4, (err, res) => {
+  if (!err) console.log('Square of 4:', res); // Should print 16
+});
+memoizedSquare(4, (err, res) => {
+  if (!err) console.log('Square of 4 again:', res); // Should print 16 (cached)
+});
+memoizedSquare(5, (err, res) => {
+  if (!err) console.log('Square of 5:', res); // Should print 25
+});
+memoizedSquare(6, (err, res) => {
+  if (!err) console.log('Square of 6:', res); // Should print 36
+});
+memoizedSquare(4, (err, res) => {
+  if (!err) console.log('Square of 4 again:', res); // Should print 16 (cached)
+});
+
+
 
 
 function memoize(fn) {
